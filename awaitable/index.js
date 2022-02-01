0dc6d6db17322ccad0ef53497b10967d978cbc01
Angel Hernandez <angel.hernandez.12@live.com>
2022-02-01 16:16:21 -0400

    Update index.js

--- awaitable/index.js
+++ awaitable/index.js
@@ -1,1 +1,17 @@
-
+async function awaitable(promise) {
+  const result = {
+    error: undefined,
+    data: undefined,
+    hasError: false,
+    hasData: false
+  } 
+  try {
+    const data = await promise
+    result.hasData = true 
+    result.data = data
+  } catch(error) {
+    result.hasError = true 
+    result.error = error
+  }
+  return result
+}
